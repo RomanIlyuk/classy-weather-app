cf21536a9b86546ef0fdc6e7cc9d672b94f93414
RomanIlyuk <ilyukroma228@gmail.com>
2026-02-02 22:40:42 +0200

    The initial state of the program is built

---
+++ src/Counter.js
@@ -0,0 +1,41 @@
+// import React from "react";
+
+// class Counter extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = { count: 5 };
+//     this.handleDec = this.handleDec.bind(this);
+//     this.handleInc = this.handleInc.bind(this);
+//   }
+
+//   handleDec() {
+//     this.setState((curState) => {
+//       return { count: curState.count - 1 };
+//     });
+//   }
+
+//   handleInc() {
+//     this.setState((curState) => {
+//       return { count: curState.count + 1 };
+//     });
+//   }
+
+//   render() {
+//     const date = new Date("june 21 2027");
+//     date.setDate(date.getDate() + this.state.count);
+
+//     return (
+//       <div>
+//         <button onClick={this.handleDec}>-</button>
+//         <span>
+//           {" "}
+//           {date.toDateString()} [{this.state.count}]
+//         </span>
+//         <button onClick={this.handleInc}>+</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Counter;
